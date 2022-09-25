16d2cfe3f3d4ccd2b78fe5d931c47d7dba709e71
san896 <sannlfernandez8@gmail.com>
2022-09-25 20:31:29 -0300

    dalee

--- PI-Food-main/api/src/routes/Type.js
+++ PI-Food-main/api/src/routes/Type.js
@@ -12,25 +12,32 @@ const { API_KEY } = process.env
 // En una primera instancia, cuando no exista ninguno, deberán precargar la base de datos con los tipos de datos indicados por spoonacular acá
 
 router.get('/', async (req, res) =>{
-    // const allTypes = await axios(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&addRecipeInformation=true&number=100`)
-    // const mapTypes = await allTypes.data.results.map(e => e.diets)
+    const allTypes = await axios(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&addRecipeInformation=true&number=100`)
+    const mapTypes = await allTypes.data.results.map(e => e.diets)
 
-    // const findCreate = await mapTypes.map(  async (arr) => {
-    //     arr.forEach(async (el) => {
-    //          await Type.findOrCreate({
-    //             where: {name: el}
-    //         })
-    //     });
-    // })
-    //if(findCreate){
-        // }
-        const findAllTypes = await Type.findAll()
-        const mapFind = findAllTypes.map(e => e.dataValues.name)
-
-        if(!mapFind.length){
-            return res.status(404).send('error diets not found')
+    const findCreate = await mapTypes.map(  async (arr) => {
+        arr.forEach(async (el) => {
+             await Type.findOrCreate({
+                where: {name: el}
+            })
+        });
+    })
+    if(findCreate){
+        res.status(202).send(findCreate)
+        }else{
+            res.status(404).send('failed types')
         }
-        return res.status(202).json(mapFind)
+        
+
+        
+        // const findAllTypes = await Type.findAll()
+        // const mapFind = findAllTypes.map(e => e.dataValues.name)
+
+        // if(!mapFind.length){
+        //     return res.status(404).send('error diets not found')
+        // }
+
+        // return res.status(202).json(mapFind)
 })
 
 module.exports =  router ;

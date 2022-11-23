311e7fac702edc03f51428f2c03590810e3c3a28
san896 <sannlfernandez8@gmail.com>
2022-11-22 23:43:19 -0300

    deploying ok

--- api/index.js
+++ api/index.js
@@ -19,10 +19,11 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+const { DB_PORT } = process.env
 
 // Syncing all the models at once.
 conn.sync({ alter: true }).then(() => {
-  server.listen(3005, () => {
-    console.log('%s listening at 3005'); // eslint-disable-line no-console
+  server.listen(DB_PORT, () => {
+    console.log(`%s listening at ${DB_PORT}`); // eslint-disable-line no-console
   });
 });

946241275a636e26ec1eefaa9113c084b7300f91
san896 <sannlfernandez8@gmail.com>
2023-04-07 20:41:17 -0300

    landing

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -4,15 +4,17 @@ import style from './Landing.module.css'
 export default function LandingPage(){
     return(
         <div className={style.general}>
+
             <div className={style.contenedor}>
 
-                <h1 className={style.header}> Welcome ! </h1>
+                <h4 className={style.header}> Welcome </h4>
+                <h1 className={style.header1}> FoodApp </h1>
 
             </div>
 
             <div className={style.contBtn}>
                     <Link to='/home'>
-                        <button className={style.btn}> Click to Enter... </button>
+                        <button className={style.btn}> Click to Enter </button>
                     </Link>
 
             </div>

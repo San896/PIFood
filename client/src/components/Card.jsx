d89965dee72827a0410305a26fc40d108a3e793c
san896 <sannlfernandez8@gmail.com>
2023-04-08 22:41:55 -0300

    cards

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -9,12 +9,13 @@ export default function Card({ name, img, types, id }) {
 
 
     return (
-        <div className={style.card}>
-            <h3 className={style.name}>{ name }</h3>
-            <h5 className={style.diets}>Diets: { types }</h5>
-            <img className={style.img} src={img || imagen2} alt='imagen failed' width='170px' height='170px'/>
+        <div className={style.card} >
+            <img className={style.img} src={img || imagen2} alt='imagen failed' />
+
+            <h3 className={style.name}>{ name }
             <Link to={'/detail/'+id} > <button className={style.btninfo}> +Info </button></Link>
-            
+            </h3>
+        
         </div>
     )
 };

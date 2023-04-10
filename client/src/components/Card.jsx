2407865d4214772b46f17139701b64d3b1b2655f
san896 <sannlfernandez8@gmail.com>
2023-04-09 21:23:41 -0300

    cards

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from 'react-router-dom';
 import style from './Card.module.css'
 import imagen2 from '../imagenes/fondo 2.2 mediano.jpg'
-
+import { BsInfoCircle } from 'react-icons/bs';
 
 export default function Card({ name, img, types, id }) {
     
@@ -10,12 +10,13 @@ export default function Card({ name, img, types, id }) {
 
     return (
         <div className={style.card} >
+            <Link to={'/detail/'+id} className={style.img}>
             <img className={style.img} src={img || imagen2} alt='imagen failed' />
+            </Link>
 
             <h3 className={style.name}>{ name }
-            <Link to={'/detail/'+id} > <button className={style.btninfo}> +Info </button></Link>
+            <Link to={'/detail/'+id} className={style.link}> <button className={style.btninfo}> <BsInfoCircle className={style.icon}/> </button></Link>
             </h3>
-        
         </div>
     )
 };
